71146ccb557126c73a077b4b5782c201ffd7c146
Micha Vegte, van der <mm.vdv@hotmail.com>
2022-07-25 14:49:41 +0200

    Tests urls

--- Gereedschapvoordetuin_Backend/urls.py
+++ Gereedschapvoordetuin_Backend/urls.py
@@ -5,6 +5,6 @@ from .views import product
 
 urlpatterns = [
     # Products
-    path('products/', product.get_product, name='get_product'),
-    path('search/<path:query>', product.search_product_with_query, name='search_product_with_query'),
+    # path('products/', product.get_product, name='get_product'),
+    # path('search/<path:query>', product.search_product_with_query, name='search_product_with_query'),
 ]
