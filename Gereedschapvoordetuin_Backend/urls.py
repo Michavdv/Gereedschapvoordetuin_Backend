2f90c38e4a5ba414d5930fe4edf4bfefbafc2921
Micha Vegte, van der <mm.vdv@hotmail.com>
2022-07-27 11:43:47 +0200

    Tests urls

--- Gereedschapvoordetuin_Backend/urls.py
+++ Gereedschapvoordetuin_Backend/urls.py
@@ -6,7 +6,6 @@ from .views import product
 urlpatterns = [
     # Products
     path('products/', product.get_product, name='get_product'),
-    path('search/<path:query>', product.search_product_with_query, name='search_product_with_query'),
 
     # Django
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
--- Gereedschapvoordetuin_Backend/views/product.py
+++ Gereedschapvoordetuin_Backend/views/product.py
@@ -46,35 +46,6 @@ def get_product(request):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-@api_view(['GET'])
-def search_product_with_query(request, query):
-    """
-    Return 25 Products based on {query}
-
-    The call returns products between a range of ({page} - 1) x 25 and ({page}) x 25
-
-    e.g. page 4 returns items 75-99
-    """
-    data = get_page_and_queryset(request)
-    if not data:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    products = []
-    query = remove_special_chars(query.lower())
-    for p in data['queryset']:
-        if remove_special_chars(p.brand.lower()).find(query) != -1 or remove_special_chars(p.product_name.lower()).find(
-                query) != -1:
-            products.append(p)
-
-    if not products:
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    search_result = {"products": products[((data['page'] - 1) * (data['amount_per_row'] * 6)):(
-            (data['amount_per_row'] * 6) * data['page'])], "amount": len(products)}
-    serializer = SearchSerializer(search_result)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
 def remove_special_chars(string, islower=False):
     # Removes the special characters, html text and when islower is True, the string will be set in lowercase
     return ''.join(filter(str.isalnum, html.unescape(
